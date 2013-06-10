ebcdde85352d7590610237d42cce96ced663c903
MacLane Wilkison <mwilkison@gmail.com>
2013-06-10 10:49:19 -0400

    add files

---
+++ temperature.py
@@ -0,0 +1,35 @@
+import webapp2
+import cgi
+
+def convertTemp(celTemp):
+    '''convert Celsius temperature to Fahrenheit temperature'''
+    if celTemp == "":
+        return ""
+    try:
+        farTemp = float(celTemp) * 9/5 + 32
+        farTemp = round(farTemp, 3)
+        return str(farTemp)
+    except ValueError:
+        return "invalid input"
+
+class MainPage(webapp2.RequestHandler):
+    def get(self):
+        celTemp = self.request.get("celTemp")
+        farTemp = convertTemp(celTemp)
+
+        self.response.headers["Content-Type"] = "text/html"
+        self.response.write("""
+	  <html>
+	    <head><title>Temperature Converter</title></head>
+	    <body>
+	      <form action="/" method="get">
+		Celsius temperature: <input type="text"
+					name="celTemp" value={}>
+		<input type="submit" value="Convert"><br>
+		Fahrenheit temperature: {}
+	      </form>
+	    </body>
+	  </html>""".format(celTemp, farTemp))
+
+routes = [('/', MainPage)]
+myApp = webapp2.WSGIApplication(routes, debug=True)
